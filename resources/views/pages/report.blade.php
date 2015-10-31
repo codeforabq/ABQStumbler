a6608d1b993a80934c7d6f37805a7cbf5dce05c5
Eric <egross00@live.com>
2015-10-31 17:20:45 -0600

    Small changes for report page

--- resources/views/pages/report.blade.php
+++ resources/views/pages/report.blade.php
@@ -9,23 +9,24 @@
 @section('content')
     {!! Form::open(['url' => 'maincs']) !!}
     <div class="form-group">
-        {!! Form::label('title', 'Title:') !!}
+        {!! Form::label('title', 'Report:') !!}
         {!! Form::text('title', null, ['class' => 'form-control']) !!}  
     </div>  
     <div class="form-group">
-        {!! Form::label('summary', 'Summary:') !!}
-        {!! Form::textarea('summary', null, ['class' => 'form-control', 'rows' => '2']) !!}  
-    </div>  
-    <div class="form-group">
-        {!! Form::label('body', 'Grumble:') !!}
-        {!! Form::textarea('body', null, ['class' => 'form-control', 'id' => 'maincbod']) !!}
+        {!! Form::label('image', 'Image:') !!}
+        {!! Form::file('image', null, ['class' => 'form-control']) !!}  
     </div>
+   <p id="demo"></p>
+    <div class="form-group">
+        {!! Form::label('location', 'Location:') !!}
+        {!! Form::text('location', null, ['class' => 'form-control', 'value' => '30']) !!}  
+    </div>  
     <div class="form-group">
         {!! Form::label('tags', 'Tags:') !!}<small>(separate with #symbol)</small>
-        {!! Form::text('tags', null, ['class' => 'form-control', 'placeholder' => 'Type tags here (e.g. #pharmacy #levothyroxine #endocrinology #cardiology)']) !!}  
+        {!! Form::text('tags', null, ['class' => 'form-control', 'placeholder' => 'Type tags here (e.g. #sidewalk #tree #bikelane)']) !!}  
     </div>  
     <div class="form-group">
-        {!! Form::submit('Submit Grumble', ['class' => 'btn btn-primary form-control subclass']) !!}
+        {!! Form::submit('Submit Report', ['class' => 'btn btn-primary form-control subclass']) !!}
     </div>
     {!! Form::close() !!}
     @if($errors->any())
@@ -38,5 +39,25 @@
 @stop
 
 @section('scripts')
+<script>
+$(function() {
+    
+    navigator.geolocation.getCurrentPosition(showPosition);
+    
+    var x = document.getElementById("demo");
 
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else { 
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+
+    function showPosition(position) {
+        x.innerHTML = "Latitude: " + position.coords.latitude + 
+        "<br>Longitude: " + position.coords.longitude;	
+    }
+});
+</script>
 @stop
